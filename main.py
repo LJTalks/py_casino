7bfd56217696231d603f1d20eeb088766aa5d44f
Laura Read <laurajmtb@gmail.com>
2024-08-30 10:39:47 +0000

    Slot machine spins

--- main.py
+++ main.py
@@ -1,8 +1,49 @@
 # https://youtu.be/th4OBktqK1I?si=M_0IxvBlsNJR0FBj
-# Enter a deposit
+import random
+
 MAX_LINES = 3
 MAX_BET = 100
 MIN_BET = 1
+
+ROWS = 3
+COLS = 3
+
+symbol_count = {
+    "A": 2,
+    "B": 4,
+    "C": 6,
+    "D": 8
+}
+
+def get_slot_machine_spin(rows, cols, symbols):
+    all_symbols = []
+    for symbol, symbol_count in symbols.items():
+        for _ in range(symbol_count):
+            all_symbols.append(symbol)
+            
+    columns = []
+    for _ in range(cols):
+        column = []
+        current_symbols = all_symbols[:]
+        for _ in range(rows):
+            value = random.choice(current_symbols)
+            current_symbols.remove(value)
+            column.append(value)
+
+        columns.append(column)
+
+    return columns
+
+def print_slot_machine(columns):
+    for row in range(len(columns[0])):
+        for i, column in enumerate(columns):
+            if i !=len(columns) -1:
+                print(column[row], end=" | ")
+            else:
+                print(column[row], end="")
+
+        print()
+
 
 def deposit():
     while True:
@@ -62,8 +103,12 @@ def main():
             print(f"You do not have enough funds, your current balance is ${balance}")
         else:
             break #could add option to increase balance/add funds here
+
     print(f"You are betting ${bet} on {lines} lines. You are betting ${total_bet} in this game")
     #Continue with the game logic
+
+    slots = get_slot_machine_spin(ROWS, COLS, symbol_count)
+    print_slot_machine(slots)
 
 
 main()
