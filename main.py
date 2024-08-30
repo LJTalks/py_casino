b9bdea3f19825cb1c13ef8bcfddc4120b0ca72ab
Laura Read <laurajmtb@gmail.com>
2024-08-30 08:42:07 +0000

    Add function to collect no of rows to bet on

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 # https://youtu.be/th4OBktqK1I?si=M_0IxvBlsNJR0FBj
 # Enter a deposit
+MAX_LINES = 3
+
 def deposit():
     while True:
         amount = input("How much would you like to deposit? $")
@@ -14,7 +16,29 @@ def deposit():
 
     return amount
 
-amount = deposit()
+def get_no_of_lines():
 
-# Cotinue with the gae logic once balance is >0
-print(f"Your balance is ${amount}. Let's play!")
+    while True:
+        lines = input("How many lines would you like to bet on? (1-" +str(MAX_LINES) + ")? ")
+        if lines.isdigit():
+            lines = int(lines)
+            if 1 <= lines <= MAX_LINES:
+                break
+            else:
+                print("Enter a valid number of lines.")
+        else:
+            print("Please enter a valid number.")
+
+    return lines
+
+def main():
+    #Get deposit amount  from user
+    balance = deposit()
+    # Print balance and proceed to game
+    print(f"Your balance is ${balance}. Let's play!")
+    # Get number of lines to bet on
+    lines = get_no_of_lines()
+    #Continue with the game logic
+    print(balance, lines)
+
+main()
